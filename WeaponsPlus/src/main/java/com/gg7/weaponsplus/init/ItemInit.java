5f78368a333bdf7c4a56a1194a3a3dd2421085f7
NickIsSwag <113379836+NickIsSwag@users.noreply.github.com>
2022-09-28 12:51:38 -0500

    added full set of tools :D
    added in an axe, hoe, pickaxe, and shovel into the game. Tweaked values to make them slightly better than diamond tools!!

--- WeaponsPlus/src/main/java/com/gg7/weaponsplus/init/ItemInit.java
+++ WeaponsPlus/src/main/java/com/gg7/weaponsplus/init/ItemInit.java
@@ -3,7 +3,11 @@ package com.gg7.weaponsplus.init;
 import com.gg7.weaponsplus.WeaponsPlus;
 import com.google.common.base.Supplier;
 
+import net.minecraft.world.item.AxeItem;
+import net.minecraft.world.item.HoeItem;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.PickaxeItem;
+import net.minecraft.world.item.ShovelItem;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.crafting.Ingredient;
@@ -21,13 +25,25 @@ public class ItemInit {
 	public static final RegistryObject<SwordItem> ECHO_SWORD = ITEMS.register("echo_diamond_sword", 
 			() -> new SwordItem(Tiers.Echo, 5, -2.0f, new Item.Properties().tab(WeaponsPlus.WeaponsPlusTab)));
 	
+	public static final RegistryObject<AxeItem> ECHO_AXE = ITEMS.register("echo_diamond_axe", 
+			() -> new AxeItem(Tiers.Echo, 6, -3.0f, new Item.Properties().tab(WeaponsPlus.WeaponsPlusTab)));
+	
+	public static final RegistryObject<PickaxeItem> ECHO_PICKAXE = ITEMS.register("echo_diamond_pickaxe", 
+			() -> new PickaxeItem(Tiers.Echo, 2, -2.8f, new Item.Properties().tab(WeaponsPlus.WeaponsPlusTab)));
+	
+	public static final RegistryObject<ShovelItem> ECHO_SHOVEL = ITEMS.register("echo_diamond_shovel", 
+			() -> new ShovelItem(Tiers.Echo, 1, -2.8f, new Item.Properties().tab(WeaponsPlus.WeaponsPlusTab)));
+	
+	public static final RegistryObject<HoeItem> ECHO_HOE = ITEMS.register("echo_diamond_hoe", 
+			() -> new HoeItem(Tiers.Echo, 0, -2.8f, new Item.Properties().tab(WeaponsPlus.WeaponsPlusTab)));
+	
 	private static <T extends Item> RegistryObject<T> register(final String name, final Supplier<T> item) {
 		return ITEMS.register(name, item);
 	}
 	
 	public static class Tiers{
 		
-		public static final Tier Echo = new ForgeTier(3, 1800, 1.0f, 2.0f, 100, null, () -> Ingredient.of(ItemInit.EXAMPLE_ITEM.get()));
+		public static final Tier Echo = new ForgeTier(3, 1800, 35.0f, 2.0f, 100, null, () -> Ingredient.of(ItemInit.EXAMPLE_ITEM.get()));
 		
 	}
 }
